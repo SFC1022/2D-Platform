289a907e6bd5859e15a1b813212396bf1e959221
SFC1022 <gwyefywgvf@naver.com>
2024-11-09 17:40:07 +0900

    2D flatpormer

---
+++ 2D platformer/Assets/Scripts/Mechanics/VictoryZone.cs
@@ -0,0 +1,31 @@
+using Platformer.Gameplay;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using static Platformer.Core.Simulation;
+
+namespace Platformer.Mechanics
+{
+    /// <summary>
+    /// Marks a trigger as a VictoryZone, usually used to end the current game level.
+    /// </summary>
+    public class VictoryZone : MonoBehaviour
+    {
+        void OnTriggerEnter2D(Collider2D collider)
+        {
+            var p = collider.gameObject.GetComponent<PlayerController>();
+            if (p != null)
+            {
+                var ev = Schedule<PlayerEnteredVictoryZone>();
+                ev.victoryZone = this;
+
+                Invoke(nameof(NextStage), 3f);
+            }
+        }
+        
+        void NextStage()
+        {
+            SceneManager.LoadScene(GameController.Instance.model.NextLevel());
+        }
+    }
+}
